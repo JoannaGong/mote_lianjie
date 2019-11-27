2096ee34307789defff62786c6fe92196d243873
Joanna0001 <gkj159@163.com>
2019-11-27 14:55:03 +0800

    d

--- index.js
+++ index.js
@@ -10,8 +10,8 @@ function openApp(openUrl, callback) {
   var isAndroid = u.indexOf('Android') > -1 || u.indexOf('Linux') > -1; //android终端或者uc浏览器
   var isIos = !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/); //ios终端
 
-  console.log('isAndroid' + isAndroid)
-  console.log('isIos' + isIos)
+  alert('isAndroid' + isAndroid)
+  alert('isIos' + isIos)
   if(isAndroid){
       openUrl = 'weibo://'
   }else if(isIos){
