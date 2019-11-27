df6f2e09d34d0f2d03dde780390e036c3fdaa878
Joanna0001 <gkj159@163.com>
2019-11-27 15:22:47 +0800

    a

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-window.onload = function(){
-  var openUrl = document.getElementById('down_app')
+window.onload = function () {
+  var openUrl = document.getElementById('down_app').href
   openApp(openUrl, false)
 }
 
@@ -10,50 +10,48 @@ function openApp(openUrl, callback) {
   var isAndroid = u.indexOf('Android') > -1 || u.indexOf('Linux') > -1; //android终端或者uc浏览器
   var isIos = !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/); //ios终端
 
-  alert('isAndroid' + isAndroid)
-  alert('isIos' + isIos)
-  if(isAndroid){
-      openUrl = 'weibo://'
-  }else if(isIos){
-      window.location.href = 'weixin://';
+  if (isAndroid) {
+    openUrl = 'weibo://'
+  } else if (isIos) {
+    window.location.href = 'weixin://';
   }
   //检查app是否打开
-  function checkOpen(cb){
-      var _clickTime = +(new Date());
-      function check(elsTime) {
-          if ( elsTime > 3000 || document.hidden || document.webkitHidden) {
-              cb(1);
-          } else {
-              cb(0);
-          }
+  function checkOpen(cb) {
+    var _clickTime = +(new Date());
+    function check(elsTime) {
+      if (elsTime > 3000 || document.hidden || document.webkitHidden) {
+        cb(1);
+      } else {
+        cb(0);
       }
-      //启动间隔20ms运行的定时器，并检测累计消耗时间是否超过3000ms，超过则结束
-      var _count = 0, intHandle;
-      intHandle = setInterval(function(){
-          _count++;
-          var elsTime = +(new Date()) - _clickTime;
-          if (_count>=100 || elsTime > 3000 ) {
-              clearInterval(intHandle);
-              check(elsTime);
-          }
-      }, 20);
+    }
+    //启动间隔20ms运行的定时器，并检测累计消耗时间是否超过3000ms，超过则结束
+    var _count = 0, intHandle;
+    intHandle = setInterval(function () {
+      _count++;
+      var elsTime = +(new Date()) - _clickTime;
+      if (_count >= 100 || elsTime > 3000) {
+        clearInterval(intHandle);
+        check(elsTime);
+      }
+    }, 20);
   }
   //在iframe 中打开APP
   var ifr = document.createElement('iframe');
   ifr.src = openUrl;
   ifr.style.display = 'none';
   if (callback) {
-      checkOpen(function (opened) {//checkOpen中的cbk参数 = function (opened)
-          if(opened == 0){
-              //用户没有安装app 可以请求下载地址并跳转 跳转方法：window.location.href 即可
-          }else if(opened ==1){
-              //用户打开了app  用户有安装app 
-          }
-      });
+    checkOpen(function (opened) {//checkOpen中的cbk参数 = function (opened)
+      if (opened == 0) {
+        //用户没有安装app 可以请求下载地址并跳转 跳转方法：window.location.href 即可
+      } else if (opened == 1) {
+        //用户打开了app  用户有安装app 
       }
+    });
+  }
   document.body.appendChild(ifr);
-  setTimeout(function() {
-      document.body.removeChild(ifr);
+  setTimeout(function () {
+    document.body.removeChild(ifr);
   }, 3000);
 }
 
