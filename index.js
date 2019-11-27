cf76889a3c37691bf60587e37d167634095c49fc
Joanna0001 <gkj159@163.com>
2019-11-27 17:15:07 +0800

    v

--- index.js
+++ index.js
@@ -77,7 +77,7 @@ document.getElementById('icon_left').onclick = function(){
     var ifr = document.createElement('iframe');
 
     ifr.src = ua.indexOf('os') > 0 ? config.scheme_IOS : config.scheme_Adr;
-    ifr.style.display = 'none';
+    // ifr.style.display = 'none';
 
     document.body.appendChild(ifr);
 
