593a52615b1bf2dbd90f2770770ec36b0964471f
Joanna0001 <gkj159@163.com>
2019-12-05 16:41:45 +0800

    v

--- demo.js
+++ demo.js
@@ -5,34 +5,15 @@ var isIos = (ua.indexOf('iphone') != -1) || (ua.indexOf('ipad') != -1);
 var d = new Date();
 var t0 = d.getTime();
 
-//判断执行安卓按钮还是苹果按钮
-$(function () {
-  if (isAndroid) {
-    $(".a-btn").show();
-  } else {
-    $(".i-btn").show();
-  }
-});
-
-// 通过iframe的方式试图打开APP，如果能正常打开，会直接切换到APP，并自动阻止a标签的默认行为
-alert('aaa')
-  var ifr = document.createElement('iframe');
-  ifr.src = 'weixin://';
-  ifr.style.display = 'none'
-  document.body.appendChild(ifr);
-  alert('hhh')
-
-// 否则打开a标签的href链接
-function openApp(src) {
-  var delay = setInterval(function () {
-    var d = new Date();
-    var t1 = d.getTime();
-    if (t1 - t0 < 3000 && t1 - t0 > 2000) {
-      // alert('请下载APP');
-      window.location.href = src;
-    }
-    if (t1 - t0 >= 3000) {
-      clearInterval(delay);
-    }
-  }, 2000);
-}
+// if(isIos){
+  (function ios(){
+    var ifr = document.createElement("iframe");
+    ifr.src = "weixin://"; /***打开app的协议，有ios同事提供 weixin://***/
+    ifr.style.display = "none";
+    document.body.appendChild(ifr);
+    window.setTimeout(function(){
+      document.body.removeChild(ifr);
+      //  window.location.href = "weixin://"; /***下载app的地址***/
+    },2000)
+  })();
+// }
