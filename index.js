bc33c401025567a671397e238eb47761e0393772
Joanna0001 <gkj159@163.com>
2019-12-05 16:58:54 +0800

    v

--- index.js
+++ index.js
@@ -1,30 +1,45 @@
+var ua = navigator.userAgent.toLowerCase();
+var isWeixin = ua.indexOf('micromessenger') != -1;
+var isAndroid = ua.indexOf('android') != -1;
+var isIos = (ua.indexOf('iphone') != -1) || (ua.indexOf('ipad') != -1);
+var open_app_url = 'wxe3e7c50449fde018://';
+var download_url;
+
+function init(){
+  switch(app){
+    case isWeixin:
+      break;
+    case isAndroid:
+      download_url = 'https://model-back.xinghaotian.cn/yiyue.apk';
+      break;
+    case isIos:
+      download_url = "https://apps.apple.com/cn/app/%E8%89%BA%E7%BA%A6/id1485342529?ign-mpt=uo%3D4";
+      break;
+  }
+
+  document.getElementById('down_app').href = download_url;
+}
+
 (function(){
   function openclient(){
-    if (navigator.userAgent.match(/(iPhone|iPod|iPad);?/i)) {  // 苹果
-      var loadDateTime = new Date();
-      window.setTimeout(function () {
-        var timeOutDateTime = new Date();
-        if (timeOutDateTime - loadDateTime < 1000) {   // 下载
-          alert('aa')
-          window.location = "https://apps.apple.com/cn/app/%E8%89%BA%E7%BA%A6/id1485342529?ign-mpt=uo%3D4";
-        } else {
-          alert('bb')
-          window.close();
-        }
-      }, 800);
-      window.location = "wxe3e7c50449fde018://";
-    } else if (navigator.userAgent.match(/android/i)) {  // 安卓
-      var state = null;
-      try {
-        state = window.open("wxe3e7c50449fde018://", '_blank');
-      } catch (e) { }
-      if (state) {
-        // alert('aa')
-        // window.close();
-      } else {
-        alert('bb')
-        window.location = "https://model-back.xinghaotian.cn/yiyue.apk";
+    var startTime = Date.now();
+    var ifr = document.createElement('iframe');
+
+    ifr.src = open_app_url;
+    ifr.style.display = 'none';
+    document.body.appendChild(ifr);
+
+    var t = setTimeout(function(){
+      var endTime = Date.now();
+      if(endTime - startTime > 1000){
+        document.body.removeChild(ifr);
+      }else{
+        window.location = download_url;
       }
+    }, 1000);
+
+    window.onblur = function(){
+      this.clearTimeout(t);
     }
   }
 
@@ -32,4 +47,5 @@
     openclient()   // 自动打开app
     document.getElementById('down_app').addEventListener('click', openclient, false)
   }, false)
+
 })()
